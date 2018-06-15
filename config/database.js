ee85c90ab54b863718d61104d06b5c98f81e78a8
Dila <dila@vurung.com>
2018-06-15 13:51:33 +0545

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// config/database.js
+
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'port': '3307',
+        'user': 'pos-user',
+        'password': 'pos'
+    },
+    'database': 'test_db',
+    'users_table': 'users'
+};
